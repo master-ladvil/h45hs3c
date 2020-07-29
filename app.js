888605c1fd13b3b23ca36a3f0f9c4e59e7d4e718
master-ladvil <52162632+master-ladvil@users.noreply.github.com>
2020-07-29 11:03:24 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const bodyparser = require("body-parser")
+require("dotenv/config")
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
+
+const dbroute = require('./routes/classifiedRoutes')
+app.use('/checkdb',dbroute)
+
+const userroute = require('./routes/user/user-auth')
+app.use('/db/users', userroute)
+
+
+app.get('/db',(req,res) => {
+    res.send("hi")
+})
+
+
+
+mongoose.connect(process.env.db_connector,
+    {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    },() => console.log("connected to db")
+    )
+
+
+
+app.listen(4500)
+console.log("listening on port : 4500")
